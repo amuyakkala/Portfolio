d590889e3f02518b3b0d6323e339a3757d9fd997
amulya yakkala <txy4896@mavs.uta.edu>
2024-07-14 15:09:17 -0500

    changes

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,8 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
+          "Software Developer",  
+          "FullStack Developer",
           "MERN Stack Developer",
           "Open Source Contributor",
         ],
